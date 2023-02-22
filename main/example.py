e38d79d3072672761e3d413e36491acf780cad2c
Twexdy <thenextcomment2@gmail.com>
2023-02-22 10:09:39 -0600

    An example of a chess bot that makes random moves.

--- main/example.py
+++ main/example.py
@@ -1,11 +1,25 @@
 import chesster as cs
-from pygame import *
+import time
+import random
+import pygame
 
-init()
-clock = time.Clock()
-main = cs.new(scale=90)
+pygame.init()
+clock = pygame.time.Clock()
+main = cs.new(scale=80, debug_mode=True)
+
+def make_move() :
+    if main.legal_moves == [] or main.animating:
+        return
+    time.sleep(0.5)
+    print(len(main.legal_moves))
+    random_move = main.legal_moves[random.randint(0, len(main.legal_moves) - 1)]
+    main.move(*random_move)
 
 while True:
     main.update()
-    display.flip()
+    pygame.display.flip()
+    
+    if main.turn == "b":
+        make_move()
+    
     clock.tick(main.refresh_rate_cap)
